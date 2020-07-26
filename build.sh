859cae2376d9defcada0c4950ad8aa1c549f0b7c
Seth Lessard <sethlessard@outlook.com>
2020-07-25 21:48:42 -0500

    Reorganizing build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 # build the kernel
+echo
+echo "========================================="
+echo "Building the Kernel"
+echo "========================================="
+echo
 make
-make boot.iso
 
 # build libc
+echo
+echo
+echo "========================================="
+echo "Building user-land libc"
+echo "========================================="
+echo
 pushd libc
 make
 popd
+
+echo
+echo
+echo "========================================="
+echo "Building the ISO"
+echo "========================================="
+echo
+make boot.iso
